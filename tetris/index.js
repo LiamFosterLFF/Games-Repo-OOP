8b9f26ffc32da9a34370175b316f4f15595f1b70
Liam Foster <Liamfoster.LFF@gmail.com>
2020-07-24 18:30:37 +0100

    Changed gravity function of tetris so speed is adjustable

--- tetris/index.js
+++ tetris/index.js
@@ -3,21 +3,39 @@ const dims = {x: 600, y: 800}
 const screen = {x: dims.x/4, y: dims.y/7, w: dims.x/2, h: dims.y *(3/4)};
 const blockDims = {w: screen.w/10, h: screen.h/20};
 var piece = null;
-const gravity = screen.h/20;
-
+let gameSpeed = 1000;
 const blockMap = new Array(20).fill(null).map((row) => new Array(10).fill(null))
 
 function setup() {
     createCanvas(dims.x, dims.y);
-    blockMap[0][5] = new Block();
+    createPiece();
+    gravity();
 }
+
+function createPiece() {
+    const pieceTypes = ["I", "J", "L", "O", "S", "Z"]
+    piece = new Piece(pieceTypes[floor(random()*pieceTypes.length)]);
+}
+
+function gravity() {
+    setInterval(dropPiece, gameSpeed)
+
+    function dropPiece() {
+        piece.y += 1;
+    }
+}
+
 
 function draw() {
     frameRate(1);
     drawScreen();
+
     // drawPiece();
     // gravityDrop();
 }
+
+
+
 
 function drawScreen() {
     background("#d8d1cf");
@@ -26,7 +44,6 @@ function drawScreen() {
     drawGrid();
     drawBlocks();
     drawPiece();
-    dropPiece();
 
     function drawGrid() {
         for (let row = 0; row < 20; row++) {
@@ -54,11 +71,7 @@ function drawScreen() {
     }
 
     function drawPiece() {
-        if (piece === null) {
-            const pieceTypes = ["I", "J", "L", "O", "S", "Z"]
-            piece = new Piece(pieceTypes[floor(random()*pieceTypes.length)]);
-        }
-        console.log(piece.shape);
+        
         for (let row = 0; row < piece.shape.length; row++) {
             for (let col = 0; col < piece.shape[row].length; col++) {
                 const square = piece.shape[row][col]
@@ -68,12 +81,14 @@ function drawScreen() {
                 }
             }
         }
+
+        if (piece.y + piece.h >= 20) {
+            blockMap[piece.y][piece.x] = new Block()
+        }
     }
 
-    function dropPiece() {
-        piece.y += 1;
-    }
 }
+
 
 
 function Block() {
@@ -138,16 +153,26 @@ function Piece(type) {
 
 
 
-// Add ability to spin pieces
-
 function keyPressed() {
-    // if (keyIsDown(LEFT_ARROW)) {
-    //     dot.x -= dot.w;
-    // } else if (keyIsDown(RIGHT_ARROW)) {
-    //     dot.x += dot.w;
-    // }
+    if (keyIsDown(LEFT_ARROW)) {
+        piece.x -= 1;
+    } else if (keyIsDown(RIGHT_ARROW)) {
+        piece.x += 1;
+    }
     if (keyIsDown(32)) {
         piece.spin();
-        drawScreen();
     }
+    drawScreen();
 }
+
+
+// PIeces can't go below bottom row
+// Pieces fit into space based on lowest piece hung on sth
+// Pieces drop 1 row /s, increasing speed as time goes on
+// Next piece shows up in a preview spot
+// Can move/drop pieces by pressing down, double drop to place now
+// If row is full, empty pieces
+// Keep score
+// GAme over, both points and liness
+// Can hold a piece
+// Predictive dropping
