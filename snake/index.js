ede8bda74ec2a126bef00badc5ccfccf04ace541
Liam Foster <Liamfoster.LFF@gmail.com>
2020-08-03 18:18:03 +0100

    Can now make hamiltonian cycles at least partially well for Snake AI, although still some small bugs

--- snake/index.js
+++ snake/index.js
@@ -54,17 +54,23 @@ function Snake() {
         const startingPoint = [[0,0], [0,1], [1,1], [1,0]];
         const startingPointNo = 0;
         const hCycle = startingPoint;
+        const addedToCycle = {0: ["whoCares"]};
         const frontier = {};
+        let counter = 0;
         primsMazeIterator(startingPoint, startingPointNo);
         
         function primsMazeIterator(point, pointNo) {
+            counter++
+            pointNo = Number(pointNo)
             const [w, h] = [width/scl, height/scl];
             for (let i = 0; i < 4; i++) {
                 const corner = point[i];
                 const [y, x] = corner
                 if (i === 0) { //Top Bar
                     if(y-1 >=0) { //Not on edge, then add to frontier (or add to existing frontier entry)
-                        if (frontier[pointNo-w] === undefined) {
+                        if (addedToCycle[pointNo-w] !== undefined) {
+
+                        } else if (frontier[pointNo-w] === undefined) {
                             frontier[pointNo-w] = ["down"];
                         } else {
                             frontier[pointNo-w].push("down");
@@ -72,8 +78,10 @@ function Snake() {
                     }
                 }
                 if (i === 1) { //Right Bar
-                    if(x+1 < 40) { //Not on edge, then add to frontier (or add to existing frontier entry)
-                        if (frontier[pointNo+1] === undefined) {
+                    if(x+1 < w) { //Not on edge, then add to frontier (or add to existing frontier entry)
+                        if (addedToCycle[pointNo+1] !== undefined) {
+
+                        } else if (frontier[pointNo+1] === undefined) {
                             frontier[pointNo+1] = ["left"];
                         } else {
                             frontier[pointNo+1].push("left");
@@ -81,8 +89,10 @@ function Snake() {
                     }
                 }
                 if (i === 2) { //Bottom Bar
-                    if(y+1 < 40) { //Not on edge, then add to frontier (or add to existing frontier entry)
-                        if (frontier[pointNo+w] === undefined) {
+                    if(y+1 < h) { //Not on edge, then add to frontier (or add to existing frontier entry)
+                        if (addedToCycle[pointNo+w] !== undefined) {
+
+                        } else if (frontier[pointNo+w] === undefined) {
                             frontier[pointNo+w] = ["up"];
                         } else {
                             frontier[pointNo+w].push("up");
@@ -91,7 +101,9 @@ function Snake() {
                 }
                 if (i === 3) { //Right Bar
                     if(x-1 >= 0) { //Not on edge, then add to frontier (or add to existing frontier entry)
-                        if (frontier[pointNo-1] === undefined) {
+                        if (addedToCycle[pointNo-1] !== undefined) {
+
+                        } else if (frontier[pointNo-1] === undefined) {
                             frontier[pointNo-1] = ["right"];
                         } else {
                             frontier[pointNo-1].push("right");
@@ -100,7 +112,14 @@ function Snake() {
                 }
             }
             const frontierCellsArr = Object.keys(frontier);
+            console.log(addedToCycle);
+            // console.log(counter);
+            if (counter > 10) {
+                return true;
+            } 
+
             const nextFrontierCell = frontierCellsArr[floor(random(frontierCellsArr.length))];
+            console.log(nextFrontierCell);
             const noOfDirChoices = frontier[nextFrontierCell].length;
             const cellConnxnDir = frontier[nextFrontierCell][floor(random(noOfDirChoices))];
             const nextFrontierCellTopLeftPoint = [floor(nextFrontierCell/w), nextFrontierCell%w]
@@ -108,46 +127,54 @@ function Snake() {
             const nextCellPoints = [nextFrontierCellTopLeftPoint, [cy, cx+1], [cy+1, cx+1], [cy+1, cx]];
 
             let cellToConnectWith = null;
-            if (cellConnxnDir === "down") {
+            if (cellConnxnDir === "down") { // Find insertion point (top left of cell in cycle) and insert first and second new points into cycle
                 cellToConnectWith = Number(nextFrontierCell) + w;
-                const cellToConnectWithTopLefttPoint = [floor(cellToConnectWith/w), cellToConnectWith%w];
+                const cellToConnectWithTopLeftPoint = [floor(cellToConnectWith/w), cellToConnectWith%w];
                 const cycleInsertionPointIndex = cycleInsertionPointIndexFinder(hCycle, cellToConnectWithTopLeftPoint);
                 hCycle.splice(cycleInsertionPointIndex+1, 0, nextCellPoints[0], nextCellPoints[1])
+                console.log("Down",nextCellPoints[0], nextCellPoints[1]);
                 
             } 
-            if(cellConnxnDir === "left") {
+            if(cellConnxnDir === "left") { // Find insertion point (top right of cell in cycle) and insert second and third new points into cycle
                 cellToConnectWith = Number(nextFrontierCell) - 1;
                 const cellToConnectWithTopRightPoint = [floor(cellToConnectWith/w), cellToConnectWith%w + 1];
                 const cycleInsertionPointIndex = cycleInsertionPointIndexFinder(hCycle, cellToConnectWithTopRightPoint);
                 hCycle.splice(cycleInsertionPointIndex+1, 0, nextCellPoints[1], nextCellPoints[2])
+                console.log("Left",nextCellPoints[1], nextCellPoints[2]);
 
             } 
-            if(cellConnxnDir === "up") {
+            if(cellConnxnDir === "up") { // Find insertion point (bottom right of cell in cycle) and insert third and fourth new points into cycle
                 cellToConnectWith = Number(nextFrontierCell) - w;
                 const cellToConnectWithBottomRightPoint = [floor(cellToConnectWith/w) + 1, cellToConnectWith%w + 1];
                 const cycleInsertionPointIndex = cycleInsertionPointIndexFinder(hCycle, cellToConnectWithBottomRightPoint);
                 hCycle.splice(cycleInsertionPointIndex+1, 0, nextCellPoints[2], nextCellPoints[3])
+                console.log("Up",nextCellPoints[2], nextCellPoints[3]);
 
             } 
-            if(cellConnxnDir === "right") {
+            if(cellConnxnDir === "right") { // Find insertion point (bottom left of cell in cycle) and insert fourth and first new points into cycle
                 cellToConnectWith = Number(nextFrontierCell) + 1;
                 const cellToConnectWithBottomLeftPoint = [floor(cellToConnectWith/w) + 1, cellToConnectWith%w];
-                const cycleInsertionPointIndex = cycleInsertionPointIndexFinder(hCycle, cellToConnectWithBottomLefttPoint);
-                hCycle.splice(cycleInsertionPointIndex+1, 0, nextCellPoints[2], nextCellPoints[0])
+                const cycleInsertionPointIndex = cycleInsertionPointIndexFinder(hCycle, cellToConnectWithBottomLeftPoint);
+                hCycle.splice(cycleInsertionPointIndex+1, 0, nextCellPoints[3], nextCellPoints[0])
+                console.log("Right",nextCellPoints[3], nextCellPoints[0]);
 
             }
-            const cellConnectionPoints = []
 
+            addedToCycle[nextFrontierCell] = frontier[nextFrontierCell];
+            delete frontier[nextFrontierCell];
+            primsMazeIterator(nextCellPoints, nextFrontierCell)
+        
         }
 
         function cycleInsertionPointIndexFinder(arr, subArr) {
             for (let i = 0; i < arr.length; i++) {
                 if (arr[i][0] === subArr[0] && arr[i][1] === subArr[1]) {
+                    console.log(i, arr[i], subArr);
                     return i;
                 }
             }
         }
-
+        console.log(hCycle);
         return hCycle;
     }
 
