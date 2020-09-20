2d7cfa37cb0aee0258fbf596109b40181cbf12d5
Liam Foster <Liamfoster.LFF@gmail.com>
2020-09-20 10:34:56 +0100

    Added column checking functionality to sudoku

--- sudoku/index.js
+++ sudoku/index.js
@@ -32,17 +32,24 @@ function keyPressed() {
         for (let i = 0; i < matchingPairs.length; i++) {
             if ((matchingPairs[i][0][0] === row && matchingPairs[i][0][1] === col) || (matchingPairs[i][1][0] === row && matchingPairs[i][1][1] === col)) {
                 matchingPairs[i] = [[null, null], [null, null]];
-                console.log(matchingPairs);
             }
             
         }
 
         //Check row
-        for (let checkCol= 0; checkCol< 10; checkCol++) {
+        for (let checkCol= 0; checkCol< 9; checkCol++) {
             if (checkCol !== col) {
                 if (board[row][checkCol] === num) {
                     matchingPairs.push([[row, col], [row,checkCol]])
-                    console.log(matchingPairs);
+                }
+            }
+        }
+
+        //Check column 
+        for (let checkRow= 0; checkRow< 9; checkRow++) {
+            if (checkRow !== row) {
+                if (board[checkRow][col] === num) {
+                    matchingPairs.push([[row, col], [checkRow,col]])
                 }
             }
         }
