70634972b20d61f9a7d93bacb32004d4e275ce67
lf639 <lf639@bath.ac.uk>
2021-04-07 20:17:31 +0100

    Checkers: Moved board functionality to OOP structure for instantiating board

--- checkers/index.js
+++ checkers/index.js
@@ -1,6 +1,7 @@
 const dim = {width: 600, height: 600}
 var cnv;
 const pieceLoc = [];
+let board;
 const offset = {x: 40, y: 40};
 const sqSize = (dim.width - 2*offset.x)/8
 let selectedSquare = [];
@@ -26,28 +27,16 @@ function draw() {
     cnv.mouseClicked(selectPiece)
 }
 
+
+
 function setPieces() {
-    let c = 1;
-    for (let row = 0; row < 8; row++) {
-        const pieceRow = [];
-        for (let col = 0; col < 8; col++) {
-            if ((row < 3 || row > 4) && (row + col) % 2 === 1) {
-                var piece = new Piece();
-                piece.color = (row < 3) ? "black" : "red";
-                piece.name = `${piece.color} ${c}`;
-                c++;
-                pieceRow.push(piece)
-            } else (
-                pieceRow.push(null)
-            )
-        }
-        pieceLoc.push(pieceRow)   
-    }
+    board = new Board();
 }
 
 
 function drawBoard() {
-    
+    const board = new Board();
+    console.log(board);
     clear();
     background(177,98,20);
     fill(255);
@@ -65,16 +54,17 @@ function drawBoard() {
             fill(sqColor)
             square(sqLoc.x, sqLoc.y, sqSize);
             
-            if (pieceLoc[row][col] !== null) {
-                const pieceColor = (pieceLoc[row][col].color === "black") ? 255 : "#da1e1e"
+            if (board.pieces[row][col] !== null) {
+                console.log(board.pieces[row][col], row, col);
+                const pieceColor = (board.pieces[row][col].color === "black") ? 255 : "#da1e1e"
                 fill(pieceColor)
                 rect(sqLoc.x, sqLoc.y, sqSize, sqSize, 50)
 
-                if ((row === 7 && pieceLoc[row][col].color === "black") || (row === 0 && pieceLoc[row][col].color === "red")) {
-                    pieceLoc[row][col].king = true;
+                if ((row === 7 && board.pieces[row][col].color === "black") || (row === 0 && board.pieces[row][col].color === "red")) {
+                    board.pieces[row][col].king = true;
                 }
 
-                if (pieceLoc[row][col].king) {
+                if (board.pieces[row][col].king) {
                     tint(pieceColor);
                     image(img, sqLoc.x +sqSize/4, sqLoc.y +sqSize/5, sqSize/2, sqSize/2);
                 }
@@ -93,33 +83,28 @@ function drawBoard() {
     }
 }
 
-function Piece() {
-    this.color = null;
-    this.name = null;
-    this.king = false;
 
-}
 
 function selectPiece() {
     const [row, col] = [floor((mouseY - offset.y)/sqSize), floor((mouseX - offset.x)/sqSize)];
 
     if ((row + col)%2 === 1) {
-        if (selectedSquare.length === 0 && pieceLoc[row][col].color === whoseTurn) {
+        if (selectedSquare.length === 0 && board.pieces[row][col].color === whoseTurn) {
             selectedSquare = [row, col];
         } else {
-            const pieceColor = (pieceLoc[selectedSquare[0]][selectedSquare[1]] === null) ? null : pieceLoc[selectedSquare[0]][selectedSquare[1]].color;
+            const pieceColor = (board.pieces[selectedSquare[0]][selectedSquare[1]] === null) ? null : board.pieces[selectedSquare[0]][selectedSquare[1]].color;
 
-            const redLegalJump = (pieceColor === "red" && selectedSquare[0] - 2 === row && abs(selectedSquare[1] - col) === 2 && pieceLoc[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2].color !== null && pieceLoc[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2].color === "black" && pieceLoc[row][col] === null);
-            const blackLegalJump = (pieceColor === "black" && selectedSquare[0] + 2 === row && abs(selectedSquare[1] - col) === 2 && pieceLoc[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2] !== null &&  pieceLoc[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2].color === "red" && pieceLoc[row][col] === null);
+            const redLegalJump = (pieceColor === "red" && selectedSquare[0] - 2 === row && abs(selectedSquare[1] - col) === 2 && board.pieces[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2].color !== null && board.pieces[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2].color === "black" && board.pieces[row][col] === null);
+            const blackLegalJump = (pieceColor === "black" && selectedSquare[0] + 2 === row && abs(selectedSquare[1] - col) === 2 && board.pieces[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2] !== null &&  board.pieces[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2].color === "red" && board.pieces[row][col] === null);
             if (isLegalMove(pieceColor, row, col)) {
-                pieceLoc[row][col] = pieceLoc[selectedSquare[0]][selectedSquare[1]];
-                pieceLoc[selectedSquare[0]][selectedSquare[1]] = null;
+                board.pieces[row][col] = board.pieces[selectedSquare[0]][selectedSquare[1]];
+                board.pieces[selectedSquare[0]][selectedSquare[1]] = null;
                 selectedSquare = [];
                 whoseTurn = (whoseTurn === "black") ? "red" : "black";
             } else if (redLegalJump || blackLegalJump) {
-                pieceLoc[row][col] = pieceLoc[selectedSquare[0]][selectedSquare[1]];
-                pieceLoc[selectedSquare[0]][selectedSquare[1]] = null;
-                pieceLoc[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2] = null;
+                board.pieces[row][col] = board.pieces[selectedSquare[0]][selectedSquare[1]];
+                board.pieces[selectedSquare[0]][selectedSquare[1]] = null;
+                board.pieces[(row+selectedSquare[0])/2][(col+selectedSquare[1])/2] = null;
                 (redLegalJump) ? score.red++ : score.black++;
                 selectedSquare = [];
                 whoseTurn = (whoseTurn === "black") ? "red" : "black";
@@ -134,23 +119,61 @@ function selectPiece() {
 
 
 function isLegalMove(color, row, col) {
-    const isKing = (pieceLoc[selectedSquare[0]][selectedSquare[1]].king);
+    const isKing = (board.pieces[selectedSquare[0]][selectedSquare[1]].king);
     if (color === "black") {
         if (selectedSquare[0] + 1 === row || (isKing && abs(selectedSquare[1] - col) === 1)
             && abs(selectedSquare[1] - col) === 1
-            && pieceLoc[row][col] === null
+            && board.pieces[row][col] === null
         ) {return true}
 
     } else if (color === "red") {
         if (selectedSquare[0] - 1 === row || (isKing && abs(selectedSquare[1] - col) === 1)
             && abs(selectedSquare[1] - col) === 1
-            && pieceLoc[row][col] === null
+            && board.pieces[row][col] === null
         ) {return true}
 
     }
     return false;
 }
 
+class Piece {
+    constructor(color) {
+        this.color = color;
+        this.king = false;    
+    }
+}
+
+
+
+class Board {
+    constructor() {
+        this.pieces = this.setPieces()
+    }
+
+    setPieces() {
+        const pieces = []
+        for (let row = 0; row < 8; row++) {
+            const boardRow = [];
+            console.log("A");
+            for (let col = 0; col < 8; col++) {
+                // Only put pieces on green squares: Odd row + even col or even row + odd col
+                if ((row + col) % 2 === 1) { 
+                    if (row < 3) {
+                        boardRow.push(new Piece("black"));
+                    } else if (row > 4) {
+                        boardRow.push(new Piece("red"));
+                    } else {
+                        boardRow.push(null)
+                    }
+                } else{
+                    boardRow.push(null)
+                }
+            }
+            pieces.push(boardRow) 
+        }
+        return pieces
+    }
+}
 //STILL TO DO:
 // double jumping
 // Win condition
